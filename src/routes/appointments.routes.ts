e3366de528dedb446b19aeba3ce086b455dbc58a
leoavi <leoavivideos@gmail.com>
2020-05-20 18:03:00 -0300

    tst
    tst

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.get('/', (request, response) => {
-  return response.json({ message: 'tst' });
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
